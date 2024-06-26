c1abdf0c29efbfcc14a88b2f17aaeb8ca457bfd1
Carlos Romero <romerocarlos0805@gmail.com>
2024-06-26 17:00:06 -0500

    feat: dynamic input for unit selection (us/metric)

---
+++ wwwroot/js/script.js
@@ -0,0 +1,21 @@
+alert("working");
+
+const usUnits = document.querySelector("#usUnits");
+const metricUnits = document.querySelector("#metricUnits");
+
+const heightUS = document.querySelector("#heightUS");
+const heightMetric = document.querySelector("#heightMetric");
+
+const weigthSpan = document.querySelector("#weightUnits");
+
+metricUnits.addEventListener("click", function(){
+    heightUS.classList.add("inactive");
+    heightMetric.classList.remove("inactive");
+    weigthSpan.textContent = "kg";
+});
+
+usUnits.addEventListener("click", function(){
+    heightUS.classList.remove("inactive");
+    heightMetric.classList.add("inactive");
+    weigthSpan.textContent = "lbs";
+});
